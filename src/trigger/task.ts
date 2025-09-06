a97a3d52ce6b2cb798d4d006244586a0ab018987
Eric Allam <eallam@icloud.com>
2025-09-06 13:07:03 +0100

    Backlink from langsmith to trigger cloud dashboard URL

--- src/trigger/task.ts
+++ src/trigger/task.ts
@@ -1,8 +1,8 @@
-import { logger, task } from "@trigger.dev/sdk";
+import { Context, logger, task } from "@trigger.dev/sdk";
 import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
-const testRun = async (input: string) => {
+const testRun = async (input: string, ctx: Context) => {
   console.log("Hello world from console.log", input);
 
   await logger.trace("testRun", async (span) => {
@@ -16,6 +16,10 @@ const testRun = async (input: string) => {
       ],
       experimental_telemetry: {
         isEnabled: true,
+        metadata: {
+          triggerRunId: ctx.run.id,
+          triggerRunUrl: `https://cloud.trigger.dev/orgs/${ctx.organization.slug}/projects/${ctx.project.slug}/env/${ctx.environment.slug}/runs/${ctx.run.id}`,
+        },
       },
     });
 
@@ -25,7 +29,7 @@ const testRun = async (input: string) => {
 
 export const testTelemetry = task({
   id: "test-telemetry",
-  run: async (payload: { input: string }) => {
-    await testRun(payload.input);
+  run: async (payload: { input: string }, { ctx }) => {
+    await testRun(payload.input, ctx);
   },
 });
